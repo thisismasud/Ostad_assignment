a405aa65edff8f6d51dca6da28b3ccc8f4885761
thisismasud <thisismasud1@gmail.com>
2023-03-23 22:43:29 +0600

    first commit

---
+++ module1.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num){
+    for(i = 1; i <=10; i++){
+        let product = num * i;
+        console.log(`${num} x ${i} = ${product}`)
+    }
+}
+multiplicationTable(9); //multiplication table of 9
+multiplicationTable(4); //multiplication table of 4
+multiplicationTable(2); //multiplication table of 2
